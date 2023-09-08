00f6837c6291390548fd4f4d26acbd36ddbdc5e5
JP Lyon <japylyon@gmail.com>
2023-09-08 13:21:51 -0500

    Random Fox

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
     $('#clicker').click(function() {
       $.ajax({
         dataType: "json",
-        url: "https://random-d.uk/api/random",
+        url: "https://randomfox.ca/floof/",
         success: function(results) {
-          console.log(results["url"]);
-          if (results["url"].endsWith(".mp4")) {
+          console.log(results["image"]);
+          if (results["image"].endsWith(".mp4")) {
             $('#dog').attr("src", "images/blank.png");
           } else {
-            $('#dog').attr("src", results["url"]);
+            $('#dog').attr("src", results["image"]);
           }
         },
         error: function(xhr,status,error) {
